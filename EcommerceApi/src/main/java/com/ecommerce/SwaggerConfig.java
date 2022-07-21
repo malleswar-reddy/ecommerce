56a59590d5e31336aad1c9fdddb470babb2e87d5
malleswar-reddy <56329087+malleswar-reddy@users.noreply.github.com>
2022-07-21 09:29:42 +0530

    springfox-swagger-ui added

---
+++ EcommerceApi/src/main/java/com/ecommerce/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.ecommerce;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+@EnableSwagger2
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ecommerce"))
+                .paths(regex("/ecommerce.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Spring Boot Swagger Example API",
+                "Spring Boot Swagger Example API for Youtube",
+                "1.0",
+                "Terms of Service",
+                new Contact("Malleswar Tech", "https://www.youtube.com",
+                        "malleswar.mca@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html"
+        );
+
+        return apiInfo;
+    }
+}
--- EcommerceApi/src/main/java/com/ecommerce/controller/HomeController.java
+++ EcommerceApi/src/main/java/com/ecommerce/controller/HomeController.java
@@ -6,6 +6,7 @@ import com.ecommerce.service.CategoryService;
 import com.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/ecommerce")
 public class HomeController {
 
     @Autowired
